8952c54c5d4fe16cb553a9360d04536408dec7b8
Jean <acalto2@gmail.com>
2023-05-01 20:59:18 +0200

    memory list view widget

--- lib/memory/memory.dart
+++ lib/memory/memory.dart
@@ -1,3 +1,4 @@
 export 'providers/memory_notifier.dart';
 export 'view/memory_page.dart';
 export 'api/memory_repository.dart';
+export 'widgets/memory_list_view.dart';
---
+++ lib/memory/widgets/memory_list_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memories_app/memory/memory.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class MemoryListView extends ConsumerWidget {
+  const MemoryListView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final MemoryNotifier = ref.watch(memoryNotifierProvider);
+
+    return MemoryNotifier.when(
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, stackTrace) => Center(
+        child: Text(
+          error.toString(),
+        ),
+      ),
+      data: (data) => MasonryGridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(data[index]['title']),
+          );
+        },
+      ),
+    );
+    return Container();
+  }
+}
